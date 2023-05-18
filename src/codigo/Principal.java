e5df7b136b8df117b747b33e6f0fd02291abee8b
Guillermo <Guillermo@DAVID-51>
2023-05-17 21:12:02 -0600

    UP

--- src/codigo/Principal.java
+++ src/codigo/Principal.java
@@ -6,16 +6,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- *
- * @author Guillermo
- */
 public class Principal {
     public static void main(String[] args) throws Exception {
         String ruta1 = "D:/NetBeansProjects/CompiladoresP/src/codigo/Lexer.flex";
         String ruta2 = "D:/NetBeansProjects/CompiladoresP/src/codigo/LexerCup.flex";
         String[] rutaS = {"-parser", "Sintax", "D:/NetBeansProjects/CompiladoresP/src/codigo/Sintax.cup"};
-        generar(ruta1,ruta2, rutaS);
+        generar(ruta1, ruta2, rutaS);
     }
     public static void generar(String ruta1, String ruta2, String[] rutaS) throws IOException, Exception{
         File archivo;
@@ -26,19 +22,19 @@ public class Principal {
         java_cup.Main.main(rutaS);
         
         Path rutaSym = Paths.get("D:/NetBeansProjects/CompiladoresP/src/codigo/sym.java");
-        if(Files.exists(rutaSym)){
+        if (Files.exists(rutaSym)) {
             Files.delete(rutaSym);
         }
         Files.move(
-                Paths.get("D:/NetBeansProjects/CompiladoresP/sym.java"),
-                Paths.get("D:/NetBeansProjects/CompiladoresP/src/codigo/sym.java")
+                Paths.get("D:/NetBeansProjects/CompiladoresP/sym.java"), 
+                Paths.get("D:/Documentos/NetBeansProjects/CompiladoresP/src/codigo/sym.java")
         );
         Path rutaSin = Paths.get("D:/NetBeansProjects/CompiladoresP/src/codigo/Sintax.java");
-        if(Files.exists(rutaSin)){
+        if (Files.exists(rutaSin)) {
             Files.delete(rutaSin);
         }
         Files.move(
-                Paths.get("D:/NetBeansProjects/CompiladoresP/Sintax.java"),
+                Paths.get("D:/NetBeansProjects/CompiladoresP/Sintax.java"), 
                 Paths.get("D:/NetBeansProjects/CompiladoresP/src/codigo/Sintax.java")
         );
     }
